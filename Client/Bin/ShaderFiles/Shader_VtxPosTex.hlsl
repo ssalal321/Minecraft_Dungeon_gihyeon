27eb8928ea54d837399b613ec52bd9b9ad15d4f2
ssalal321 <140492420+ssalal321@users.noreply.github.com>
2025-03-12 22:18:20 +0900

    [필터 정리]

---
+++ Client/Bin/ShaderFiles/Shader_VtxPosTex.hlsl
@@ -0,0 +1,72 @@
+
+matrix      g_WorldMatrix, g_ViewMatrix, g_ProjMatrix;
+texture2D   g_Texture;
+
+sampler g_LinearSampler = sampler_state 
+{
+    Filter = MIN_MAG_MIP_LINEAR;
+    AddressU = CLAMP;
+    AddressV = CLAMP;
+};
+
+struct VS_IN
+{
+    float3 vPosition : POSITION;
+    float2 vTexcoord : TEXCOORD0;
+};
+
+struct VS_OUT
+{
+    float4 vPosition : SV_POSITION;
+    float2 vTexcoord : TEXCOORD0;    
+};
+
+// 리턴값 VS_MAIN(float3 vPosition : POSITION, float2 vTexcoord : TEXCOORD0)
+VS_OUT VS_MAIN(VS_IN In)
+{
+    /* 기타 변환들을 수행한다.*/   
+    VS_OUT Out = (VS_OUT)0;
+    
+    matrix matWV, matWVP;
+    
+    matWV = mul(g_WorldMatrix, g_ViewMatrix);
+    matWVP = mul(matWV, g_ProjMatrix);
+    
+    Out.vPosition = mul(vector(In.vPosition, 1.f), matWVP);
+    Out.vTexcoord = In.vTexcoord;
+    
+    return Out;
+}
+
+struct PS_IN
+{
+    float4 vPosition : SV_POSITION;
+    float2 vTexcoord : TEXCOORD0;    
+};
+
+struct PS_OUT
+{
+    vector vColor : SV_TARGET0;    
+};
+
+
+PS_OUT PS_MAIN(PS_IN In)
+{
+    PS_OUT Out = (PS_OUT) 0;
+    
+    Out.vColor = g_Texture.Sample(g_LinearSampler, In.vTexcoord);
+    
+    return Out;    
+}
+
+technique11 DefaultTechnique
+{
+    pass DefaultPass
+    {
+        VertexShader = compile vs_5_0 VS_MAIN();
+        PixelShader = compile ps_5_0 PS_MAIN();
+    }
+
+
+}
+
